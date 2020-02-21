48d4a37e729a7bfbadf4f327fc4b7e0e3162db3a
nitishpratap <54417889+nitishpratap@users.noreply.github.com>
2020-02-21 11:59:13 +0530

    Add files via upload

---
+++ conditional statement/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    int n;
+    cin>>n;
+    if(n==1)
+        cout<<"one"<<endl;
+    else if(n==2)
+    {
+        cout<<"two"<<endl;
+    }
+    else if(n==3)
+        cout<<"three"<<endl;
+    if(n==4)
+        cout<<"four"<<endl;
+    if(n==5)
+        cout<<"five"<<endl;
+    if(n==6)
+        cout<<"six"<<endl;
+    if(n==7)
+        cout<<"seven"<<endl;
+    if(n==8)
+        cout<<"eight"<<endl;
+    if(n==9)
+        cout<<"nine"<<endl;
+    else if(n>9)
+        cout<<"Greater than 9"<<endl;
+    return 0;
+}
